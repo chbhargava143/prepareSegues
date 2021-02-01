549c8be6f437a7072c17121684f9c3a4a79951d9
bhargava <chbhargava4@gmail.com>
2021-02-01 20:02:03 +0530

    send data using segue

--- prepareSegues/ViewController.swift
+++ prepareSegues/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txt_Field:UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func didTapNextVc(_ sender : UIButton){
+performSegue(withIdentifier: "detailVC", sender: self)
 
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailVC" {
+            guard let vc = segue.destination as? blueVc else {return}
+            vc.nameText = txt_Field.text ?? ""
+            
+        }
+        
+    }
 
 }
 
---
+++ prepareSegues/blueVc.swift
@@ -0,0 +1,23 @@
+//
+//  blueVc.swift
+//  sideHome
+//
+//  Created by bhargava on 30/01/21.
+//  Copyright © 2021 bhargava. All rights reserved.
+//
+
+import UIKit
+
+class blueVc: UIViewController {
+    @IBOutlet weak var txtLbl : UILabel!
+    var nameText = String()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        txtLbl.text = nameText
+        // Do any additional setup after loading the view.
+    }
+   
+   
+    
+
+}
